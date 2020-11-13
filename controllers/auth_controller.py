a485a1351e17d065cde0d8db45a0b114d230ed1f
Christopher Joseph <alfonsoj93@hotmail.co.uk>
2020-11-13 12:45:16 +1100

    JWT token

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,25 +1,23 @@
-from flask import Blueprint, abort, jsonify, request
-from schemas.UserSchema import user_schema
 from models.User import User
+from schemas.UserSchema import user_schema
 from main import db
 from main import bcrypt
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+from flask import Blueprint, request, jsonify, abort
 
-auth = Blueprint("auth",  __name__,  url_prefix="/auth")
+auth = Blueprint('auth', __name__, url_prefix="/auth")
 
-@auth.route("/register", methods=["POST"])
-def auth_register():
+@auth.route("/login", methods=["POST"])
+def auth_login():
     user_fields = user_schema.load(request.json)
 
     user = User.query.filter_by(email=user_fields["email"]).first()
 
-    if user:
-        return abort(400, description="Email already registered")
+    if not user or not bcrypt.check_password_hash(user.password, user_fields["password"]):
+        return abort(401, description="Incorrect username and password")
     
-    user = User()
-    user.email = user_fields["email"]
-    user.password = bcrypt.generate_password_hash(user_fields["password"]).decode("utf-8")
+    expiry = timedelta(days=1)
+    access_token = create_access_token(identity=str(user.id), expires_delta=expiry)
 
-    db.session.add(user)
-    db.session.commit()
-
-    return jsonify(user_schema.dump(user))
+    return jsonify({ "token": access_token })
